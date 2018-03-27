ba2c15a2ba6cbc14b9750beccbc3601d1a96dd4c
Jim <jimftw@hotmail.com>
2018-03-27 11:15:50 -0400

    cleaning up code, less duplication

--- wp-content/themes/testTheme/page-past-events.php
+++ wp-content/themes/testTheme/page-past-events.php
@@ -1,14 +1,12 @@
-<?php get_header(  ); ?>
+<?php get_header(  );
 
-<div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Past Events</h1>
-    <div class="page-banner__intro">
-      <p>This is Friction U's Past Events</p>
-    </div>
-  </div>
-</div>
+pageBanner(array(
+  'title' => 'Past Events',
+  'subtitle' => 'This is Friction U&apos;s Past Events',
+));
+?>
+
+
 
 <div class="container container--narrow page-section">
   <?php
