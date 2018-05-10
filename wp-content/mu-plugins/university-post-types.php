9edc5075e2d116079a100e8f9bed5f21cb0d53be
Jim <jimftw@hotmail.com>
2018-05-10 16:02:51 -0400

    asdas

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -28,7 +28,7 @@ function university_post_types(){
 
   register_post_type( 'event', array(
     'capability_type' => 'event',
-    'map_meta_cap' => 'true',
+    'map_meta_cap' => true,
     'supports' => array(
       'title', 'editor', 'excerpt', 'custom-fields'
     ),
